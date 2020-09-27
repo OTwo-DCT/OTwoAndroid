ba5f57cc9ef7c2d8bafc5ada4b8c9c0c89418949
Suman Mitra <sumanmitra97@gmail.com>
2020-09-27 14:07:41 +0530

    in between

--- app/src/main/java/org/gautammahapatra/otwo/Dashboard.java
+++ app/src/main/java/org/gautammahapatra/otwo/Dashboard.java
@@ -2,7 +2,16 @@ package org.gautammahapatra.otwo;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
@@ -10,7 +19,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.ParcelUuid;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -19,9 +30,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -30,24 +43,86 @@ public class Dashboard extends AppCompatActivity {
     RecyclerView recyclerView;
     List<DashboardDataBinder> dashboardDataBinderList;
     DashboardAdapter adapter;
-    Button scanButton;
+    private final String SERVICE_UUID = "94595bde-fd10-46c9-bfb0-a4073e62aede";
 
     private BluetoothAdapter bluetoothAdapter;
     private static final long LE_SCAN_PERIOD_MILLI = 30000;
     private BluetoothManager bluetoothManager;
     private BluetoothLeScanner bluetoothLeScanner;
+    private final String CHARE_UUID = "3fec3f7f-240c-4ed6-a7a3-b6a7e939794c";
+    private final String TAG = "BROADCAST###";
+    Button scanButton, broadcastButton;
     private boolean mScanning;
-    private Handler handler = new Handler();
+    AdvertiseCallback callback = new AdvertiseCallback() {
+        @Override
+        public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+            Log.d(TAG, "BLE advertisement added successfully");
+        }
+
+        @Override
+        public void onStartFailure(int errorCode) {
+            Log.e(TAG, "Failed to add BLE advertisement, reason: " + errorCode);
+        }
+    };
+    BluetoothGattServerCallback gatCallback = new BluetoothGattServerCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            super.onConnectionStateChange(device, status, newState);
+        }
+
+        @Override
+        public void onServiceAdded(int status, BluetoothGattService service) {
+            super.onServiceAdded(status, service);
+        }
+
+        @Override
+        public void onCharacteristicReadRequest(BluetoothDevice device, int requestId, int offset, BluetoothGattCharacteristic characteristic) {
+            super.onCharacteristicReadRequest(device, requestId, offset, characteristic);
+        }
+
+        @Override
+        public void onCharacteristicWriteRequest(BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
+            super.onCharacteristicWriteRequest(device, requestId, characteristic, preparedWrite, responseNeeded, offset, value);
+        }
+
+        @Override
+        public void onDescriptorWriteRequest(BluetoothDevice device, int requestId, BluetoothGattDescriptor descriptor, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
+            super.onDescriptorWriteRequest(device, requestId, descriptor, preparedWrite, responseNeeded, offset, value);
+        }
+
+        @Override
+        public void onDescriptorReadRequest(BluetoothDevice device, int requestId, int offset, BluetoothGattDescriptor descriptor) {
+            super.onDescriptorReadRequest(device, requestId, offset, descriptor);
+        }
+
+        @Override
+        public void onNotificationSent(BluetoothDevice device, int status) {
+            super.onNotificationSent(device, status);
+        }
+
+        @Override
+        public void onMtuChanged(BluetoothDevice device, int mtu) {
+            super.onMtuChanged(device, mtu);
+        }
+
+        @Override
+        public void onExecuteWrite(BluetoothDevice device, int requestId, boolean execute) {
+            super.onExecuteWrite(device, requestId, execute);
+        }
+    };
+    private Handler handler;
     private ScanCallback leScanCallback =
             new ScanCallback() {
                 @Override
                 public void onScanResult(int callbackType, ScanResult result) {
                     super.onScanResult(callbackType, result);
+                    byte[] scanRecord = Objects.requireNonNull(result.getScanRecord()).getBytes();
                     BluetoothDevice device = result.getDevice();
                     int signalStrength = result.getRssi();
                     String deviceName = device.getName();
                     DashboardDataBinder binder = new DashboardDataBinder(signalStrength, deviceName, "BLE");
                     AddCard(binder);
+                    Log.d(TAG, deviceName + ":" + hex(scanRecord));
                 }
             };
 
@@ -57,32 +132,11 @@ public class Dashboard extends AppCompatActivity {
         bluetoothAdapter.cancelDiscovery();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
-        assert bluetoothManager != null;
-        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        assert bluetoothManager != null;
-        bluetoothAdapter = bluetoothManager.getAdapter();
-        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-        scanButton = findViewById(R.id.scan_btn);
-        mScanning = false;
-        scanButton.setOnClickListener(v -> {
-            scanLeDevice();
-            Log.d("Dashboard", "Start LE Discovery");
-        });
-        try {
-            dashboardDataBinderList = new ArrayList<>();
-            adapter = new DashboardAdapter(this, dashboardDataBinderList);
-            recyclerView = findViewById(R.id.recycler_view);
-            recyclerView.setAdapter(adapter);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.d("Dashboard", Objects.requireNonNull(e.getMessage()));
-        }
+    public static byte[] asBytes(UUID uuid) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+        return bb.array();
     }
 
     private void RemoveCard(AtomicInteger index) {
@@ -132,9 +186,101 @@ public class Dashboard extends AppCompatActivity {
         }
     }
 
+    public static String hex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte aByte : bytes) {
+            result.append(String.format("%02x", aByte));
+        }
+        return result.toString();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        assert bluetoothManager != null;
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        assert bluetoothManager != null;
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+        scanButton = findViewById(R.id.scan_btn);
+        broadcastButton = findViewById(R.id.broadcast_btn);
+        broadcastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startBroadcast();
+            }
+        });
+        mScanning = false;
+        handler = new Handler();
+        scanButton.setOnClickListener(v -> {
+            scanLeDevice();
+            Log.d("Dashboard", "Start LE Discovery");
+        });
+        try {
+            dashboardDataBinderList = new ArrayList<>();
+            adapter = new DashboardAdapter(this, dashboardDataBinderList);
+            recyclerView = findViewById(R.id.recycler_view);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("Dashboard", Objects.requireNonNull(e.getMessage()));
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+    }
+
+    private void startBroadcast() {
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                .setConnectable(true)
+                .setTimeout(0)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .build();
+
+        AdvertiseData advertiseData = setAdvertiseData();
+
+        AdvertiseData scanResponseData = new AdvertiseData.Builder()
+                .addServiceUuid(new ParcelUuid(UUID.fromString(SERVICE_UUID)))
+                .setIncludeTxPowerLevel(true)
+                .build();
+
+        BluetoothLeAdvertiser bluetoothLeAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+        bluetoothLeAdvertiser.startAdvertising(settings, advertiseData, scanResponseData, callback);
+
+        BluetoothGattServer bluetoothGattServer = bluetoothManager.openGattServer(this, gatCallback);
+        BluetoothGattService service = new BluetoothGattService(UUID.fromString(SERVICE_UUID), BluetoothGattService.SERVICE_TYPE_PRIMARY);
+
+        BluetoothGattCharacteristic characteristic = new BluetoothGattCharacteristic(UUID.fromString(CHARE_UUID), BluetoothGattCharacteristic.PROPERTY_READ, BluetoothGattCharacteristic.PERMISSION_READ);
+
+        service.addCharacteristic(characteristic);
+
+        bluetoothGattServer.addService(service);
+    }
+
+    protected AdvertiseData setAdvertiseData() {
+        AdvertiseData.Builder mBuilder = new AdvertiseData.Builder();
+        ByteBuffer mManufacturerData = ByteBuffer.allocate(24);
+        byte[] uuid = asBytes(UUID.fromString(SERVICE_UUID));
+        mManufacturerData.put(0, (byte) 0xBE); // Beacon Identifier
+        mManufacturerData.put(1, (byte) 0xAC); // Beacon Identifier
+        for (int i = 2; i <= 17; i++) {
+            mManufacturerData.put(i, uuid[i - 2]); // adding the UUID
+        }
+        mManufacturerData.put(18, (byte) 0xff); // first byte of Major
+        mManufacturerData.put(19, (byte) 0xff); // second byte of Major
+        mManufacturerData.put(20, (byte) 0xff); // first minor
+        mManufacturerData.put(21, (byte) 0xff); // second minor
+        mManufacturerData.put(22, (byte) 0xB5); // txPower
+        mBuilder.addManufacturerData(224, mManufacturerData.array()); // using google's company ID
+        mBuilder.setIncludeDeviceName(false);
+        mBuilder.setIncludeTxPowerLevel(false);
+        return mBuilder.build();
     }
 }
